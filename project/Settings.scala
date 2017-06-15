27ec1193aa14ba49a42b36f1dcd3cea1e0f3ceef
Jozsef Hegedus <jhegedus42@gmail.com>
2017-06-15 17:44:05 +0200

    still not compiles...

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,19 +1,13 @@
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
-import sbt.Keys._
 import sbt._
+
 
 object Settings {
 
-//  val ex=exclude("com.chuusai","shapeless")
-  /** The name of your application */
   val name = "scalajs-spa"
 
-  /** The version of your application */
   val version = "1.1.4"
 
-//  val targetDir = (classDirectory in Compile).value / "web"
-
-  /** Options for the scala compiler */
   val scalacOptions = Seq(
     "-Xlint",
     "-unchecked",
@@ -24,28 +18,18 @@ object Settings {
 
   object versions {
     val scala = "2.11.8"
-
   }
 
-  val scalaTest = Seq("org.scalatest" %% "scalatest" % "3.0.1" % "test",
-                      "org.specs2" %% "specs2-core" % "3.8.9" % "test")
-
-  val circeVersion = "0.7.0"
+  val circeVersion = "0.8.0"
 
   val sharedDependencies = Def.setting(
-    (Seq( ) ++ Seq(
+    (Seq(
         "io.circe" %%% "circe-core",
         "io.circe" %%% "circe-generic",
         "io.circe" %%% "circe-parser"
       ).map(_ % circeVersion)))
 
-  /** Dependencies only used by the JVM project */
-  val jvmDependencies = Def.setting(
-    (Seq( ) ++ scalaTest))
+  val jvmDependencies = Def.setting( (Seq( ) ))
 
-  /** Dependencies only used by the JS project (note the use of %%% instead of %%) */
-  val scalajsDependencies = Def.setting(
-    Seq(
-      "org.scalatest" %%% "scalatest" % "3.0.1" % "test"
-    ))
+  val scalajsDependencies = Def.setting( Seq( ))
 }
