44485c53e18701038e853322071074f9be21c616
Jozsef Hegedus <jhegedus42@gmail.com>
2017-06-16 00:57:51 +0200

    compiles

--- shared/src/main/scala/app/shared/Errors.scala
+++ shared/src/main/scala/app/shared/Errors.scala
@@ -4,5 +4,13 @@ sealed trait SharedError2 {
   val description: String
 }
 
+
+import io.circe._, io.circe.generic.semiauto._
+
 case class InvalidUUIDError2(description: String)  extends SharedError2
 
+object SharedError2 // does not help
+{
+
+  implicit val fooDecoder: Decoder[SharedError2] = deriveDecoder[SharedError2]
+}
